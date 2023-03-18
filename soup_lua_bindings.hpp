8d459d50a1ed42e50ebcc374e99761c79e6d37cd
Sainan <sainan@calamity.gg>
2023-03-18 12:54:29 +0100

    Fix deprecation warning

--- soup_lua_bindings.hpp
+++ soup_lua_bindings.hpp
@@ -229,7 +229,7 @@ namespace soup
 		static int lua_json_decode(lua_State* L)
 		{
 			// String -> JSON Tree
-			auto root = json::decodeForDedicatedVariable(luaL_checkstring(L, 1));
+			auto root = json::decode(luaL_checkstring(L, 1));
 			// JSON Tree -> Lua Table
 			pushFromJson(L, *root);
 			return 1;
